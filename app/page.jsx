d624411adea132c13b6d60c90d1b8de1a1f8fad2
Oliversoder06 <lexrudolf06@gmail.com>
2024-06-20 11:57:18 +0200

    Redesigned custom cursor + bugfix with navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-// pages/page.jsx
 "use client";
 import { useState, useEffect } from 'react';
 import Header from '../components/Header';
@@ -6,9 +5,24 @@ import TopOfPage from '../components/TopOfPage';
 import LoadingScreen from '../components/LoadingScreen';
 import FlareCursor from '@/components/FlareCursor';
 
+const BrightnessSlider = ({ onChange }) => {
+  return (
+    <div className="z-[100] flex items-center no-cursor">
+      <input
+        type="range"
+        min="0"
+        max="100"
+        defaultValue="0" // Set the default position to be all the way to the left
+        className="custom-slider no-cursor"
+        onChange={(e) => onChange(parseInt(e.target.value))}
+      />
+    </div>
+  );
+};
+
 export default function Home() {
   const [loading, setLoading] = useState(true);
-  const [brightness, setBrightness] = useState(100);
+  const [brightness, setBrightness] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 3000);
@@ -20,11 +34,15 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div className='no-cursor'>
       <FlareCursor />
-      <div className={`min-h-screen ${loading ? 'overflow-hidden' : ''}`} style={{ filter: `brightness(${brightness}%)` }}>
-        {loading && <LoadingScreen />}
+      {loading && <LoadingScreen />}
+      <div className={`min-h-screen ${loading ? 'overflow-hidden' : ''}`}>
         <Header onBrightnessChange={handleBrightnessChange} />
+        <div
+          className="background-layer"
+          style={{ filter: `brightness(${brightness}%)` }}
+        ></div>
         <div className="relative z-10 pt-[64px]"> {/* Adjust padding to prevent content from going under the fixed header */}
           <TopOfPage />
         </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,26 +1,20 @@
-import React, { useState } from 'react';
-import BrightnessSlider from './BrightnessSlider';
+import React from 'react';
 import Navbar from './Navbar';
-const Header = () => {
-    const [brightness, setBrightness] = useState(0);
-    const handleBrightnessChange = (value) => {
-        setBrightness(value);
-    };
+import BrightnessSlider from './BrightnessSlider';
+
+const Header = ({ onBrightnessChange }) => {
     return (
-        <div className='w-full flex justify-center pt-5'>
+        <div className='w-full flex justify-center pt-5 no-cursor fixed z-[100]'>
             <div className='flex justify-center max-w-[1440px]'>
                 <div className='flex'>
-                    <BrightnessSlider onChange={handleBrightnessChange} />
+                    <BrightnessSlider onChange={onBrightnessChange} />
                 </div>
-                <div
-                    className="absolute inset-0 z-0"
-                    style={{ backgroundColor: '#121212', filter: `brightness(${brightness}%)` }}
-                ></div >
                 <div className='flex'>
                     <Navbar />
                 </div>
-            </div >
+            </div>
         </div>
     );
 };
+
 export default Header;
