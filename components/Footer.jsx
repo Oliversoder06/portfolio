d8871fbb838f52d2a76edfa3721a44cf6fb3e69c
Oliversoder06 <lexrudolf06@gmail.com>
2024-06-25 13:03:53 +0200

    Website is now fully responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <div className='w-full mt-20'>
+        <div className='w-full mt-64'>
             <h1 className="text-white font-Monda text-5xl lg2:text-6xl text-center my-20">Let's work <span className='text-theme-blue'>Together</span></h1>
 
             {/* ALL CONTENT */}
@@ -17,8 +17,8 @@ const Footer = () => {
                     <Input placeholder='Enter your full name' label='Full Name' type='text' />
                     <Input placeholder='Enter your email address' label='Email' type='email' />
                     <div className='flex flex-col gap-2'>
-                        <label htmlFor="message" className='text-white font-Monda text-2xl cursor-none HoverCursor ml-2'>Message</label>
-                        <textarea name="" id="message" placeholder='Enter your message...' className='w-[300px] xs2:w-[500px] h-[215px] rounded-md border-[1px] border-[#ffffff] bg-[rgba(7,7,7,0.2)] cursor-none HoverCursor pl-4 pt-3 text-white text-[20px] placeholder:text-[rgba(255,255,255,0.20)] focus:border-theme-blue outline-none'></textarea>
+                        <label htmlFor="message" className='text-white font-Monda text-xl xs:text-2xl cursor-none HoverCursor ml-2'>Message</label>
+                        <textarea name="" id="message" placeholder='Enter your message...' className='w-[300px] xs2:w-[500px] h-[215px] rounded-md border-[1px] border-[#ffffff] bg-[rgba(7,7,7,0.2)] cursor-none HoverCursor pl-4 pt-3 text-white text-[16px] xs:text-[20px] placeholder:text-[rgba(255,255,255,0.20)] focus:border-theme-blue outline-none'></textarea>
                     </div>
                     <button className='w-[280px] h-[70px] rounded-[16px] border-[2px] border-[#272727] bg-[rgba(21,21,21,0.40)] HoverCursor hover:bg-[rgba(34,34,34,0.4)] hover:scale-[1.02] transition-all duration-300'>Send Message</button>
                 </form>
--- components/Input.jsx
+++ components/Input.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Input = ({ placeholder, label, type }) => {
     return (
         <div className='flex flex-col gap-2'>
-            <label htmlFor={label} className='text-white font-Monda text-2xl cursor-none HoverCursor ml-2'>{label}</label>
-            <input type={type} placeholder={placeholder} id={label} className='w-[300px] xs2:w-[500px] h-[67px] rounded-md border-[1px] border-[#ffffff] bg-[rgba(7,7,7,0.2)] cursor-none HoverCursor pl-4 text-white text-[20px] placeholder:text-[rgba(255,255,255,0.20)] focus:border-theme-blue outline-none' />
+            <label htmlFor={label} className='text-white font-Monda text-xl xs:text-2xl cursor-none HoverCursor ml-2'>{label}</label>
+            <input type={type} placeholder={placeholder} id={label} className='w-[300px] xs2:w-[500px] h-[67px] rounded-md border-[1px] border-[#ffffff] bg-[rgba(7,7,7,0.2)] cursor-none HoverCursor pl-4 text-white text-[16px] xs:text-[20px] placeholder:text-[rgba(255,255,255,0.20)] focus:border-theme-blue outline-none' />
         </div>
     )
 }
